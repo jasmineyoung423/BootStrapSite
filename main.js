476ef166108688bd5c59fb59d3c27b41e5443225
Jasmine Young <46699147+jasmineyoung423@users.noreply.github.com>
2019-04-01 18:35:03 -0600

    finished site

--- main.js
+++ main.js
@@ -8,25 +8,6 @@ $(document).ready(function() {
     });
     console.log($videoSrc);
     
-      
-      
-    // when the modal is opened autoplay it  
-    $('#AnimationModal').on('shown.bs.modal', function (e) {
-        
-    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-    $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
-    })
-      
-    
-    
-    // stop playing the youtube video when I close the modal
-    $('#AnimationModal').on('hide.bs.modal', function (e) {
-        // a poor man's stop video
-        $("#video").attr('src',$videoSrc); 
-    }) 
-
-
-
     // when the modal is opened autoplay it  
     $('#FeedModal').on('shown.bs.modal', function (e) {
         
@@ -39,7 +20,7 @@ $(document).ready(function() {
         // stop playing the youtube video when I close the modal
     $('#FeedModal').on('hide.bs.modal', function (e) {
         // a poor man's stop video
-        $("#video").attr('src',$videoSrc); 
+        $("#video").attr('src',$videoSrc);
     }) 
         
         
